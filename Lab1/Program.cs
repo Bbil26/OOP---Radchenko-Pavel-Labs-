2946b8d45444aa88586c2b3bec5bedc34e0fca5b
Bbil <Bbil@DESKTOP-0566D5L>
2023-10-06 17:07:57 +0300

    lab1 - edits lab2 - new

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -1,11 +1,12 @@
 ﻿using System;
+using System.Threading.Tasks;
 
 Console.WriteLine("Работу Выполнил\nСтудент группы ИНС-21-1\nРадченко П.П.");
 
 for (int i = 0; i < 20; i++) 
     Console.Write("__");
 
-Console.WriteLine("\nВариант 6 – Задание 1\n");
+Console.WriteLine("\nЛабораторная работа №1\nВариант 6 – Задание 1\n");
 Console.WriteLine("Пользователь вводит случайные числа,\nзатем программа выводит список чисел, в котором:\n" +
     " 0 - больше Сред.Ариф.\n 1 - меньше Сред.Ариф.\n-1 - в отсальных случаях");
 
@@ -15,30 +16,82 @@ for (int i = 0; i < 20; i++)
 Console.WriteLine("\nДля продолжения нажмите на любую кнопку...");
 
 Console.ReadKey();
-Console.Clear();
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-
-Console.WriteLine("Введите числа через пробел...");
-
-Obj task = new();
-
-Console.WriteLine();
-foreach (int item in task.arrNum)
+while (true)
 {
-    if (item > task.medNum)
-        Console.Write(0 + " ");
-    else if (item < task.medNum)
-        Console.Write(1 + " ");
-    else 
-        Console.Write(-1 + " ");
+    Console.Clear();
+    Console.WriteLine("1 - Статический метод\n2 - Экземплярный метод");
+
+    char userKey = Console.ReadKey().KeyChar;
+    Console.Clear();
+
+    if (userKey == '2')
+    {
+        Console.WriteLine("Введите числа через пробел...");
+
+        Obj task = new();
+
+        Console.WriteLine();
+        foreach (int item in task.arrNum)
+        {
+            if (item > task.medNum)
+                Console.Write(0 + " ");
+            else if (item < task.medNum)
+                Console.Write(1 + " ");
+            else
+                Console.Write(-1 + " ");
+        }
+        
+        Console.ReadKey();
+        
+        Console.Clear();
+        Console.WriteLine("Повторить? 1/0");
+        if (Console.ReadKey().KeyChar == '1')
+            continue;
+        else
+            break;
+    }
+
+    if (userKey == '1')
+    {
+        List<int> listNumsStatic = ObjStat.inputNum();
+
+        Console.WriteLine("Изначальный массив: \n");
+        foreach(int item in listNumsStatic)
+        {
+            Console.Write($"{item, -3} ");
+        }
+
+        Console.WriteLine("\n\nИзмененный массив: \n");
+        foreach (int item in ObjStat.outputNum(listNumsStatic))
+        {
+            Console.Write($"{item, -3} ");
+        }
+        
+        Console.ReadKey();
+        
+        Console.Clear();
+        Console.WriteLine("Повторить? 1/0");
+        if (Console.ReadKey().KeyChar == '1')
+            continue;
+        else
+            break;
+    }
+    else
+    {
+        continue;
+    }
 }
+
+/// /////////////////////////////////////////////////////////////////////////////////////
 
 class Obj
 {
     public float medNum;
     public int countNum;
     public List<int> arrNum;
-    
+
     public Obj()
     {
         arrNum = new();
@@ -61,5 +114,35 @@ class Obj
 
         medNum = arrNum.Sum() / arrNum.Count;
     }
-
 }
+
+public static class ObjStat
+{
+    public static List<int> inputNum()
+    {
+        Random s = new Random();
+        List<int> arrStat = new List<int>();
+
+        for (int i = 0; i < 10; i++)
+            arrStat.Add(s.Next() % 31);
+
+        return arrStat;
+    }
+
+    public static List<int> outputNum(List<int> arrNum)
+    {
+        string str = string.Empty;
+        for(int i = 0; i < arrNum.Count; i++)
+        {
+            if (arrNum[i] > arrNum.Average())
+                arrNum[i] = 0;
+            else if (arrNum[i] < arrNum.Average())
+                arrNum[i] = 1;
+            else
+                arrNum[i] = -1;
+        }
+        return arrNum;
+    }
+}
+
+
---
+++ Lab2/Program.cs
@@ -0,0 +1,199 @@
+﻿using System.Reflection;
+
+Console.WriteLine("Работу Выполнил\nСтудент группы ИНС-21-1\nРадченко П.П.");
+
+for (int i = 0; i < 20; i++)
+    Console.Write("__");
+
+Console.WriteLine("\nЛабораторная работа №2\nВариант 6 – Задание 1\n");
+Console.WriteLine("Электронный журнал учебного заведения");
+
+for (int i = 0; i < 20; i++)
+    Console.Write("__");
+
+Console.WriteLine("\nДля продолжения нажмите на любую кнопку...");
+
+Console.ReadKey();
+Console.Clear();
+///////////////////////////////////////////////////////////////////
+///
+
+string lineMainWindow = "1 - Добавить нового ученика\n" +
+                        "2 - Изменить оценки по предмету\n" +
+                        "3 - Удалить ученика\n" + 
+                        "4 - Вывести список класса\n" +
+                        "0 - Выход из программы";
+
+string lineSubjects = "1 - Физика\n" +
+                      "2 - Русский язык\n" +
+                      "3 - Математика";
+
+string lineMarks = "1 - Добавить оценки\n" +
+                   "2 - Изменить оценки";
+
+//////////////////////////////////////////////////////////////////
+///
+List<Student> listTask = new List<Student>();
+
+int flagFinalProgram = 0;
+int id = 0;
+
+while (flagFinalProgram == 0)
+{
+    Console.Clear();
+
+    Console.WriteLine(lineMainWindow);
+    char userKey = Console.ReadKey().KeyChar;
+    switch (userKey)
+    {
+        case '1' :
+            {
+                Console.Clear();
+
+                string tempLine = "";
+                string name = string.Empty;
+                string className = string.Empty;
+                IEnumerable<int> markMath= new List<int>();
+                IEnumerable<int> markPhys = new List<int>();
+                IEnumerable<int> markRus = new List<int>();
+                
+                Console.Write("Введите имя ученика...\n> "); 
+                name = Console.ReadLine();
+
+                Console.Write("Введите класс...\n> ");
+                className = Console.ReadLine();
+
+                Console.Write("Введите оценки через пробел (без запятых) " +
+                    "\nпо Математике (можно оставить пустым)...\n> ");
+                tempLine = Console.ReadLine();
+                if (tempLine != "")
+                    markMath = tempLine.Split(' ').Select(int.Parse).ToArray();
+
+                Console.Write("Введите оценки через пробел (без запятых) " +
+                    "\nпо Физике (можно оставить пустым)...\n> ");
+                tempLine = Console.ReadLine();
+                if (tempLine != "")
+                    markPhys = tempLine.Split(' ').Select(int.Parse).ToArray();
+
+                Console.Write("Введите оценки через пробел (без запятых) " +
+                    "\nпо Русскому языку (можно оставить пустым)...\n> ");
+                tempLine = Console.ReadLine();
+                if (tempLine != "")
+                    markRus = tempLine.Split(' ').Select(int.Parse).ToArray();
+
+                Console.WriteLine("Введенные данные верны? 1/0");
+
+                if (Console.ReadKey().KeyChar == '1')
+                {
+                    listTask.Add(new Student(id, name, className, markMath, markRus, markPhys));
+                    id++;
+                    Console.WriteLine(listTask[0].getStudentInfo());
+                    Console.ReadKey();
+                }
+                break;
+            }
+        case '2':
+            {
+                int currentID;
+                int currnetOperation;
+
+                Console.Clear();
+                Console.WriteLine("Введите ID ученика\n>");
+                
+
+                if (int.TryParse(Console.ReadLine(), out currentID) && listTask.Exists(x => x.ID == currentID))
+                {
+                    Console.WriteLine(lineSubjects);
+                    switch (Console.ReadKey().KeyChar)
+                    {
+                        case '1': 
+                            {
+                                var l = listTask.Where(x => x.ID == currentID);
+                                break; 
+                            }
+                        case '2':
+                            {
+
+                                break;
+                            }
+                        case '3':
+                            {
+
+                                break;
+                            }
+                        default:
+                            {
+                                Console.WriteLine("Введен неверный номер предметва!");
+                                break;
+                            }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Введено не число!");
+                    Console.ReadKey();
+                }
+                
+                break;
+            }
+        case '3':
+            {
+                break;
+            }
+        case '4':
+            {
+                break;
+            }
+        case '0' : 
+            flagFinalProgram = 1; break;
+    }
+}
+
+
+
+class Student
+{
+    public int ID { get; }
+    string Name;
+    string ClassName;
+    public IEnumerable<int> markMath { get; set; }
+    public IEnumerable<int> markRusLang { get; set; }
+    public IEnumerable<int> markPhys { get; set; }
+
+    public Student(int id, string name, string classname, IEnumerable<int> markmath, IEnumerable<int> markrus, IEnumerable<int> markphys )
+    {
+        ID = id;
+        Name = name;
+        ClassName = classname;
+        markMath = markmath;
+        markRusLang = markrus;
+        markPhys = markphys;
+    }
+
+    public string getStudentInfo()
+    {
+        double avgRus;
+        double avgPhys;
+        double avgMath;
+
+        if (markRusLang.Count() != 0)
+            avgRus = markRusLang.Average();
+        else 
+            avgRus = 0;
+
+        if (markPhys.Count() != 0)
+            avgPhys = markPhys.Average();
+        else
+            avgPhys = 0;
+
+        if (markMath.Count() != 0)
+            avgMath = markMath.Average();
+        else
+            avgMath = 0;
+
+        return $"ID: {ID}\n" + $"Имя: {Name}\t" + $"Класс: {ClassName}\n" +
+            $"Средний Балл по Математике: {avgMath}\n" +
+             $"Средний Балл по Русскому языку: {avgRus}\n" +
+              $"Средний Балл по Физике: {avgPhys}";
+    }
+}
